63977fa6e5ed0fb4488d94994ec866929e6a3ff7
WIN 10 <WIN 10@DESKTOP-ALDP739>
2024-04-25 11:27:00 -0300

    luis y su hola mundo

---
+++ src/PRINSIPAL.java
@@ -0,0 +1,11 @@
+
+public class PRINSIPAL {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String cadena = ( "HOLA MUNDO" );
+				System.out.println(cadena.concat(""));
+	}
+
+}
